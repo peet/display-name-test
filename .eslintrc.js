3ff4dd0f000f9bea538fedf3b7caae7439b33752
Peet <peter.goddard@contactpartners.com>
2016-09-19 11:47:53 +0100

    working and not working examples

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2015,
+  },
+  plugins: ['react'],
+  rules: {
+    'react/display-name': 'error',
+  },
+};
